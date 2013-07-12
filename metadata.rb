b023e8e1a8038a646511338c5f41605c57328ec7
David Ackerman <david.s.ackerman@gmail.com>
2013-07-12 11:41:17 -0600

    Basic cookbook stub.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'ssh_proxy_gateway'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures ssh_proxy_gateway'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
