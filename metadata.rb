e60443c698554300df56a5b581aefbfdd6526592
David Ackerman <david.s.ackerman@gmail.com>
2013-07-24 18:16:31 -0600

    Just skip over gateway configuration if none is specified.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'devops@cybera.ca'
 license          'All rights reserved'
 description      'Installs/Configures ssh_proxy_gateway'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 
 depends "cybera"
