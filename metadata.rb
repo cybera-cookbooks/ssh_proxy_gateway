f5343a3dda4050d20b666f12e266b56bac1d6097
David Ackerman <david.s.ackerman@gmail.com>
2013-09-12 10:10:24 -0600

    Lock in dependency.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'devops@cybera.ca'
 license          'All rights reserved'
 description      'Installs/Configures ssh_proxy_gateway'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.2'
+version          '0.3.0'
 
-depends "cybera"
+depends "cybera", "~> 0.3.0"
